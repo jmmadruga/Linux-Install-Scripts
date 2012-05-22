e6375cfe65d88dd05556b48cc3936c08a108ca54
Jerônimo Medina Madruga <jeronimo.madruga@gmail.com>
2012-05-22 18:50:03 -0300

    Added git to the development tools, changed the mscorefonts to the top so it bothers right at the start of the procedure, and remover w32codecs as it made this script unsuitable for 64 bit systems.

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ apt-get install -f -y
 sudo -E wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && sudo apt-get --quiet update && sudo apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring && sudo apt-get --quiet update
 
 # Extras do Ubuntu (codecs, flash, java, outros)
-apt-get install ubuntu-restricted-extras -y
+apt-get install ubuntu-restricted-extras ttf-mscorefonts-installer -y
 
 # Desenvolvimento
-apt-get install build-essential geany spe kompozer quickly anjuta anjuta-extras -y
+apt-get install build-essential geany spe kompozer quickly anjuta anjuta-extras git-core git-gui git-doc -y
 
 # Escritório
-apt-get install ttf-mscorefonts-installer homebank openclipart libreoffice libreoffice-l10n-pt-br workrave pyroom qalculate tesseract-ocr tesseract-ocr-por ocrfeeder gscan2pdf gnucash lo-menubar calibre focuswriter lyx impressive zim -y
+apt-get install homebank openclipart libreoffice libreoffice-l10n-pt-br lo-menubar workrave pyroom qalculate tesseract-ocr tesseract-ocr-por ocrfeeder gscan2pdf gnucash calibre lyx impressive zim -y
 
 # Multimidia
-apt-get install audacious vlc non-free-codecs w32codecs libdvdcss2 ffmpeg mencoder arista frei0r-plugins devede winff audacity brasero-cdrkit openshot gaupol dvdrip sound-juicer gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ffmpeg easytag furiusisomount cheese bombono-dvd -y
+apt-get install audacious vlc non-free-codecs libdvdcss2 ffmpeg mencoder arista frei0r-plugins devede winff audacity brasero-cdrkit openshot gaupol dvdrip sound-juicer gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ffmpeg easytag furiusisomount cheese bombono-dvd -y
 
 # Efeitos do Audacity
 apt-get install vco-plugins tap-plugins swh-plugins rev-plugins omins mcp-plugins ladspa-sdk csladspa cmt caps blop blepvco amb-plugins -y
@@ -28,7 +28,7 @@ apt-get install vco-plugins tap-plugins swh-plugins rev-plugins omins mcp-plugin
 apt-get install gimp gimp-gmic inkscape python-uniconvertor scribus blender shutter trimage pencil dia vym darktable -y
 
 # GIMP 2.8
-add-apt-repository ppa:otto-kesselgulasch/gimp && apt-get update && apt-get install gimp
+add-apt-repository ppa:otto-kesselgulasch/gimp && apt-get update && apt-get install gimp -y
 
 # Compactadores e descompactadores
 apt-get install arj lha p7zip p7zip-full p7zip-rar rar unrar unace-nonfree unp -y
@@ -43,5 +43,4 @@ apt-get install chromium-browser chromium-browser-l10n lynx midori pidgin msn-pe
 apt-get install hplip-cups -y
 
 # Aplicativos a remover
-
-apt-get purge gwibber* empathy* thunderbird*
+apt-get purge gwibber* empathy* thunderbird* -y
