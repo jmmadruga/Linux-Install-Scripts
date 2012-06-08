e8f1c4b15356d9aaf2ab0f8680fb4a597a1556b2
Jerônimo Medina Madruga <jeronimo.madruga@gmail.com>
2012-06-07 21:46:20 -0300

    Added the last GPL header...

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright 2012 Jerônimo Medina Madruga
+#
+# This file is part of Linux Install Scripts.
+# Linux Install Scripts is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# Linux Install Scripts is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Linux Install Scripts. If not, see http://www.gnu.org/licenses/
+
 # Virar root antes de tudo :)
 sudo su
 
